64d1b77a3fa7b348be60f84e31bd7fbe4567f5b2
Imam Kusniadi <imamkusniadi5@gmail.com>
2020-01-07 08:30:21 +0700

    Fixing

--- system/core/class_upload.php
+++ system/core/class_upload.php
@@ -10,35 +10,37 @@ class upload {
         $file_type      = $_FILES[$name]['type'];
         $file_tmp       = $_FILES[$name]['tmp_name'];
         $file_extension = strtolower(end(explode('.',$file_name)));
-        $upload_path    = $path . basename($file_name); 
+        $new_file_name  = sha1(time()) . '.' . $file_extension;
+        $upload_path    = $path . basename($new_file_name); 
 
-        if (isset($_POST['submit'])) {
-            if (!in_array($file_extension, $this->file_extensions)) {
-                $this->errors[] = "This file extension is not allowed.";
-            }
-            if ($fileSize > 20000000) {
-                $this->errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 20MB";
-            }
-            if (empty($this->errors)) {
-                $upload = move_uploaded_file($file_tmp, $upload_path);
-                if ($upload) {
-                    return array(
-                        "status" => true,
-                        "path"   => $path,
-                        "file"   => basename($file_name)
-                    );
-                } else {
-                    return array(
-                        "status" => false,
-                        "errors" => "An error occurred somewhere. Try again or contact the admin"
-                    );
-                }
+        if(empty($file_name)) {
+            $this->errors[] = "No image selected to upload.";
+        }
+        if (!in_array($file_extension, $this->file_extensions)) {
+            $this->errors[] = "This file extension is not allowed.";
+        }
+        if ($fileSize > 20000000) {
+            $this->errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 20MB.";
+        }
+        if (empty($this->errors)) {
+            $upload = move_uploaded_file($file_tmp, $upload_path);
+            if ($upload) {
+                return array(
+                    "status" => true,
+                    "path"   => $path,
+                    "file"   => basename($new_file_name)
+                );
             } else {
                 return array(
                     "status" => false,
-                    "errors" => $this->errors
-                );
-            }
+                    "errors" => "An error occurred somewhere. Try again or contact the admin"
+                    );
+                }
+        } else {
+            return array(
+                "status" => false,
+                "errors" => $this->errors
+            );
         }
     }
 
